7dfd6d8862af5477fb0e344d87255728c836700d
eatvegg <projpython1@gmail.com>
2022-03-18 20:38:53 +0530

    second

---
+++ todo_app/views.py
@@ -0,0 +1,39 @@
+
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+
+from .forms import Todoforms
+from .models import Task
+
+
+# Create your views here.
+
+
+def task_view(request):
+    obj1 = Task.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        priority = request.POST.get('priority')
+        obj = Task(name=name, priority=priority)
+        obj.save()
+    return render(request, 'task_view.html', {'obj1': obj1})
+
+
+def delete(request, taskid):
+    task = Task.objects.get(id=taskid)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('/')
+    return render(request, 'delete.html', {'task': task})
+
+
+
+def update(request,id):
+    task=Task.objects.get(id=id)
+    form=Todoforms(request.POST or None,instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'edit.html',{'task':task,'form':form})
